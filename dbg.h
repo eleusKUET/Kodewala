1aead27e5fe9a7ec48105f86ae6df0460d4995cf
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2024-11-19 22:31:13 +0600

    Update dbg.h

--- dbg.h
+++ dbg.h
@@ -23,32 +23,17 @@ void _print() {}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 
-vector<string> vec_splitter(string s) {
-    s += ',';
-    vector<string> res;
-    while(!s.empty()) {
-        res.push_back(s.substr(0, s.find(',')));
-        s = s.substr(s.find(',') + 1);
-    }
-    return res;
-}
-void debug_out(
-vector<string> __attribute__ ((unused)) args,
-__attribute__ ((unused)) int idx, 
+vector<string> vec_splitter(string s) {s += ','; vector<string> res; 
+while(!s.empty()) { res.push_back(s.substr(0, s.find(','))); s = s.substr(s.find(',') + 1);}
+return res;}
+void debug_out(vector<string> __attribute__ ((unused)) args,__attribute__ ((unused)) int idx, 
 __attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
 template <typename Head, typename... Tail>
 void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
-    if(idx > 0) cerr << ","; else cerr << "Line(" << LINE_NUM << "): ";
-    cerr << args[idx] << " = "; _print(H);
-    debug_out(args, idx + 1, LINE_NUM, T...);
-}
+if(idx > 0) cerr << ","; else cerr << "Line(" << LINE_NUM << "): ";cerr << args[idx] << " = "; _print(H);
+debug_out(args, idx + 1, LINE_NUM, T...);}
 
-#ifndef XOX
 #define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
-#else
-#define debug(...) 42
-#endif
-
 
 int main() {
     vector<int> a = {1, 2, 3};
