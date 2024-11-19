9f8aaa0b2fadbdc1f6712911768560a1b4905a65
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2024-11-19 16:56:11 +0600

    Update hopcroftKarp.cpp

--- hopcroftKarp.cpp
+++ hopcroftKarp.cpp
@@ -1,75 +1,98 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N = 3e5 + 9;
+#include <random>
 
-struct HopcroftKarp {
-  static const int inf = 1e9;
-  int n;
-  vector<int> l, r, d;
-  vector<vector<int>> g;
-  HopcroftKarp(int _n, int _m) {
-    n = _n;
-    int p = _n + _m + 1;
-    // n = no of nodes, m = extension for v + n;
-    g.resize(p);
-    l.resize(p, 0);
-    r.resize(p, 0);
-    d.resize(p, 0);
-  }
-  void add_edge(int u, int v) {
-    g[u].push_back(v + n); //right id is increased by n, so is l[u]
-  }
-  bool bfs() {
-    queue<int> q;
-    for (int u = 1; u <= n; u++) {
-      if (!l[u]) d[u] = 0, q.push(u);
-      else d[u] = inf;
-    }
-    d[0] = inf;
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      for (auto v : g[u]) {
-        if (d[r[v]] == inf) {
-          d[r[v]] = d[u] + 1;
-          q.push(r[v]);
+template <bool ToShuffle = false>
+struct bipartite_matching {
+    int n_left, n_right, flow = 0;
+    std::vector<std::vector<int>> g;
+    std::vector<int> match_from_left, match_from_right;
+
+    bipartite_matching(int _n_left, int _n_right)
+        : n_left(_n_left),
+          n_right(_n_right),
+          g(_n_left),
+          match_from_left(_n_left, -1),
+          match_from_right(_n_right, -1),
+          dist(_n_left) {}
+
+    void add(int u, int v) { g[u].push_back(v); }
+
+    std::vector<int> dist;
+
+    void bfs() {
+        std::queue<int> q;
+        for (int u = 0; u < n_left; ++u) {
+            if (!~match_from_left[u])
+                q.push(u), dist[u] = 0;
+            else
+                dist[u] = -1;
         }
-      }
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (auto v : g[u])
+                if (~match_from_right[v] && !~dist[match_from_right[v]]) {
+                    dist[match_from_right[v]] = dist[u] + 1;
+                    q.push(match_from_right[v]);
+                }
+        }
     }
-    return d[0] != inf;
-  }
-  bool dfs(int u) {
-    if (!u) return true;
-    for (auto v : g[u]) {
-      if(d[r[v]] == d[u] + 1 && dfs(r[v])) {
-        l[u] = v;
-        r[v] = u;
-        return true;
-      }
+
+    bool dfs(int u) {
+        for (auto v : g[u])
+            if (!~match_from_right[v]) {
+                match_from_left[u] = v, match_from_right[v] = u;
+                return true;
+            }
+        for (auto v : g[u])
+            if (dist[match_from_right[v]] == dist[u] + 1 &&
+                dfs(match_from_right[v])) {
+                match_from_left[u] = v, match_from_right[v] = u;
+                return true;
+            }
+        return false;
     }
-    d[u] = inf;
-    return false;
-  }
-  int maximum_matching() {
-    int ans = 0;
-    while (bfs()) {
-      for(int u = 1; u <= n; u++) if (!l[u] && dfs(u)) ans++;
+
+    int get_max_matching() {
+        if constexpr (ToShuffle) {
+            random_device rd;
+            mt19937 mt(rd());
+            for (int i = 0; i < n_left; ++i)
+                std::random_shuffle(std::begin(g[i]), std::end(g[i]), mt);
+        }
+        while (true) {
+            bfs();
+            int augment = 0;
+            for (int u = 0; u < n_left; ++u)
+                if (!~match_from_left[u]) augment += dfs(u);
+            if (!augment) break;
+            flow += augment;
+        }
+        return flow;
     }
-    return ans;
-  }
+
+    std::pair<std::vector<int>, std::vector<int>> minimum_vertex_cover() {
+        std::vector<int> L, R;
+        for (int u = 0; u < n_left; ++u) {
+            if (!~dist[u])
+                L.push_back(u);
+            else if (~match_from_left[u])
+                R.push_back(match_from_left[u]);
+        }
+        return {L, R};
+    }
+
+    std::vector<std::pair<int, int>> get_edges() {
+        std::vector<std::pair<int, int>> ans;
+        for (int u = 0; u < n_left; ++u)
+            if (match_from_left[u] != -1)
+                ans.emplace_back(u, match_from_left[u]);
+        return ans;
+    }
 };
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  int n, m, q;
-  cin >> n >> m >> q;
-  HopcroftKarp M(n, m);
-  while (q--) {
-    int u, v;
-    cin >> u >> v;
-    M.add_edge(u, v);
-  }
-  cout << M.maximum_matching() << '\n';
-  return 0;
+
+signed main() {
+  
 }
